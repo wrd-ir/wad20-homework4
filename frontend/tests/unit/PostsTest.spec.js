5ec1d666cb3a7d2e474cc286f7af307ace1b5a02
Janne  Sokk <Janne.sokk@ttu.ee>
2020-12-10 14:11:45 +0200

    Test if media property then image or video is rendered

--- frontend/tests/unit/PostsTest.spec.js
+++ frontend/tests/unit/PostsTest.spec.js
@@ -109,4 +109,10 @@ describe('Posts', () => {
         expect(testData.length).toEqual(renderedElements.length)
     });
 
+    it('renders an media property if there is one', () => {
+        const renderedElements = wrapper.findAll('.post-image');
+        const mediaElements = testData.filter(item => item.media);
+        expect(mediaElements.length).toEqual(renderedElements.length)
+    })
+
 });
